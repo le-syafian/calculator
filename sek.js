28baf5f8a30fd906f52d0dd62ce557866e652586
le-syafian <syafian00@gmail.com>
2025-12-20 19:05:35 +0700

    belajar popup box

---
+++ sek.js
@@ -0,0 +1,26 @@
+alert ('ini adalah program sederhana kecocokan cinta');
+
+var data1 = false;
+var data2 = false;
+
+while (data1 === false) {
+    var nama1 = prompt ('siapa nama kamu?');
+    var cekBoolean = Boolean(nama1);
+    data1 = cekBoolean;
+}
+
+while (data2 === false) {
+    var nama2 = prompt ('siapa nama dia?');
+    var cekBoolean2 = Boolean(nama2);
+    data2 = cekBoolean2;
+}
+
+var hasilconfirm = confirm ('apakah ' + nama2 + ' mencintai ' + nama1 + ' ?');
+
+if (hasilconfirm === true) {
+    alert ('kamu cinta dia selamat');
+} else {
+    alert ('kasihan :(');
+}
+
+ console.log (INFO);
