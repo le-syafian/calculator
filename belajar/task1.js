84952e938444937eb244db8bbaddfbc14519b52f
le-syafian <syafian00@gmail.com>
2025-12-21 15:57:19 +0700

    belajar while dan for

--- belajar/task1.js
+++ belajar/task1.js
@@ -1,6 +1,15 @@
 var nomorAngkot = 1;
-while (nomorAngkot <= 10) {
+var jmlAngkot = 50;
+while (nomorAngkot <= jmlAngkot) {
     console.log("Angkot No. " + nomorAngkot + " beroperasi dengan baik.");
     nomorAngkot++;
 }
-console.log("Semua angkot telah beroperasi dengan baik.");
+console.log("Semua angkot telah beroperasi dengan baik. ini pakai while");
+
+
+var nomorAngkot1 = 51;
+var jmlAngkot1 = 100;
+for (nomorAngkot1;nomorAngkot1 <= jmlAngkot1; nomorAngkot1++) {
+    console.log('Angkot No. ' + nomorAngkot1 + ' beroperasi dengan baik.');
+}
+console.log('Semua angkot telah beroperasi dengan baik. ini pakai for'); 
