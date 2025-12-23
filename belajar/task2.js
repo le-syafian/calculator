62847053e0c2dfdef0c703cdf01be939df954064
le-syafian <syafian00@gmail.com>
2025-12-23 11:05:10 +0700

    belajar for

---
+++ belajar/task2.js
@@ -0,0 +1,11 @@
+var jmlAngkot = 15;
+var nomorAngkot =1;
+var angkotBeroperasi = 5;
+while (nomorAngkot <= angkotBeroperasi) {
+    console.log('Angkot No.' + nomorAngkot + ' beroperasi dengan baik.')
+nomorAngkot++
+}
+for (nomorAngkot ; nomorAngkot <= jmlAngkot ; nomorAngkot++) {
+    console.log('angkot no.' + nomorAngkot + ' sedang tidak beroperasi')
+}
+console.log('semua angkot sudah di cek.');
